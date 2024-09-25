c97e9b46d085fbc40123d207a27b83dc8fb9075f
roger0n <gurogel@gmail.com>
2024-09-25 10:14:58 -0300

    aoba

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './imagens/logo.svg';
 import './App.css';
+import logo from './imagens/logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className='logo'>
-          <img src= {logo} alt ='logo'></img>
+          <img src={logo} alt='logo'></img>
           <p><strong>Store</strong>Books</p>
         </div>
       </header>
